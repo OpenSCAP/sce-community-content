15ed1296fa17e17c7f937efb32402a20de016ec4
Martin Preisler <mpreisle@redhat.com>
2012-04-18 12:53:10 +0200

    Removed "argparse" dependency in xccdf_merge.py, it isn't available in stock Python 2.6

--- utils/xccdf_merge.py
+++ utils/xccdf_merge.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python2
 
-import argparse
-from xml.etree import ElementTree
-parser = argparse.ArgumentParser(description = "Merges 1 or more XCCDFs (just Rules and Groups) into given template XCCDF")
-parser.add_argument("template", type = str,
-                    help = "The template XCCDF that the merge results will be put into")
-parser.add_argument("globs", metavar = "F", type = str, nargs = "+",
-                   help = "File or glob of an XCCDF to merge in")
-
-args = parser.parse_args()
-
+import sys
 import glob
-files = []
-for g in args.globs:
-    gfiles = glob.glob(g)
-    files.extend(gfiles)
-    if len(gfiles) == 0:
-        print("No file matches '%s'" % (g))
+from xml.etree import ElementTree
 
-template_dom = None
-with open(args.template, "r") as file:
-    template_dom = ElementTree.fromstring(file.read())
+def merge_files(template, files):
+    template_dom = None
+    with open(template, "r") as file:
+        template_dom = ElementTree.fromstring(file.read())
 
-for file_path in files:
-    with open(file_path, "r") as file:
-        dom = ElementTree.fromstring(file.read())
-        
-        for child in dom.findall("*"):
-            if child.tag.endswith("Group") or child.tag.endswith("Rule"):
-                template_dom.append(child)
-        
-print(ElementTree.tostring(template_dom, "utf-8"))             
+    for file_path in files:
+        with open(file_path, "r") as file:
+            dom = ElementTree.fromstring(file.read())
+            
+            for child in dom.findall("*"):
+                if child.tag.endswith("Group") or child.tag.endswith("Rule"):
+                    template_dom.append(child)
+            
+    return ElementTree.tostring(template_dom, "utf-8")
+
+def process(template, globs):
+    files = []
+    for g in globs:
+        gfiles = glob.glob(g)
+        files.extend(gfiles)
+        if len(gfiles) == 0:
+            print("No file matches '%s'" % (g))
+
+    return merge_files(template, files)
+
+def main():
+    if len(sys.argv) < 3:
+        print("xccdf_merge.py TEMPLATE FILE [FILE] ..")
+    else:
+        template = sys.argv[1]
+        files = sys.argv[2:]
+
+        print(process(template, files))
+
+if __name__ == "__main__":
+    main()
+
+
