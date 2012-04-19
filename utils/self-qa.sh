4ca3b22116e3829ad0a9ffb3af43a996a0f1d660
Martin Preisler <mpreisle@redhat.com>
2012-04-19 13:04:17 +0200

    Human readable titles for STIG checks

--- utils/self-qa.sh
+++ utils/self-qa.sh
@@ -21,7 +21,7 @@ make > /dev/null
 
 for file in $INPUT_XCCDF $ALL_XCCDF; do
     xmllint --schema "$XCCDF_SCHEMA" $file > /dev/null
-    
+
     missing_check_import=$(xpath $file "//check[not(check-import[@import-name='stdout'])]" 2> /dev/null)
     if [[ "$missing_check_import" != "" ]]; then
         echo "stdout check-import is missing in $file, details follow:"
@@ -30,4 +30,14 @@ for file in $INPUT_XCCDF $ALL_XCCDF; do
     fi
 done
 
+missing_stig_idents=$(xpath STIG-xccdf.xml "//Rule[not(ident[@system='http://iase.disa.mil/stigs'])]" 2> /dev/null)
+if [[ "$missing_stig_idents" != "" ]]; then
+    echo "at least one STIG check is missing an <ident> entry, details follow:"
+    echo $missing_stig_idents
+    echo
+fi
+
+# TODO: Check that STIG checks ident match check-content-ref/@href
+
 popd > /dev/null
+
