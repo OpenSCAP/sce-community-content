1a4fd45b8ba58536f200ca0540c7ea5b52649270
Viktor Hercinger <vhercing@redhat.com>
2013-02-21 11:02:13 +0100

    added guide.html to install script

--- src/install.sh
+++ src/install.sh
@@ -3,6 +3,7 @@
 DESTDIR=$1
 
 install -D -m 644 all-resolved-xccdf.xml $DESTDIR/usr/share/openscap/sce-community-content.xml
+install -D -m 644 guide.html $DESTDIR/usr/share/doc/sce-community-content-1.0/guide.html
 while read path
 do
   install -D -m 755 $path $DESTDIR/usr/share/openscap/$path
