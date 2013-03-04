c61733ee6f064cfd7df177d1158b9504b79dac54
Viktor Hercinger <vhercing@redhat.com>
2013-03-04 13:57:19 +0100

    moved remediation scripts into the XML
    this way they'll show up in the HTML guide

--- src/services/openssh/NISPOM-SSHD_Disable_Empty_Passwords_fix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Check if PermitEmptyPasswords is enabled (commented lines will be replaced too)
-if grep -q PermitEmptyPasswords /etc/ssh/sshd_config
-then
-  sed -i 's|^.*PermitEmptyPasswords.*$|PermitEmptyPasswords no|ig' /etc/ssh/sshd_config
-else
-  echo 'PermitEmptyPasswords no' >> /etc/ssh/sshd_config
-fi
--- src/services/openssh/NISPOM-SSHD_Disable_Root_Login_fix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Check if PermitRootLogin is enabled (commented lines will be replaced too)
-if grep -q PermitRootLogin /etc/ssh/sshd_config
-then
-  sed -i 's|^.*PermitRootLogin.*$|PermitRootLogin no|ig' /etc/ssh/sshd_config
-fi
