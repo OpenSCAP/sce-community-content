00015543a8de272b4d8d8411af84466e710f0e4e
Viktor Hercinger <vhercing@redhat.com>
2013-03-28 14:24:27 +0100

    updated auth content checks (added rules, fixes, etc.)

--- src/system/authcontent/08_pam.sh
+++ src/system/authcontent/08_pam.sh
@@ -5,7 +5,6 @@
 # Adapted for SCE by Martin Preisler <mpreisle@redhat.com>
 
 SYSTEM_AUTH_CFG="/etc/pam.d/system-auth"
-PARANOID=0
 
 if [[ ! -f ${SYSTEM_AUTH_CFG} ]]; then
     echo "Configuration file \"${SYSTEM_AUTH_CFG}\" doesn't exist."
@@ -16,19 +15,16 @@ RET=$XCCDF_RESULT_PASS
 
 sed -n -e ':a; /\\$/N; s/\\\n//; ta; s/^[ \t]*password[^#]*pam_\(cracklib\|pwquality\).so.*//; T; q 1' ${SYSTEM_AUTH_CFG}
 if [[ "$?" -eq "0" ]]; then
-	echo "New passwords for system services aren't checked with cracklib or pwquality."
-	echo "Add a call to pam_cracklib or pam_pwquality to '${SYSTEM_AUTH_CFG}'. Please see 'man pam_cracklib' or 'man pam_pwquality' for details."
-	RET=$XCCDF_RESULT_INFORMATIONAL
+    echo "New passwords for system services aren't checked with cracklib or pwquality."
+    echo "Add a call to pam_cracklib or pam_pwquality to '${SYSTEM_AUTH_CFG}'. Please see 'man pam_cracklib' or 'man pam_pwquality' for details."
+    RET=$XCCDF_RESULT_FAIL
 fi
 
-if (( ${PARANOID} == 1 )); then
-    sed -n -e ':a; /\\$/N; s/\\\n//; ta; s/^[ \t]*auth[^#]*pam_tally\.so.*//p; T; q 1' ${SYSTEM_AUTH_CFG}
-    if [[ "$?" -eq "0" ]]; then
-	echo "Account lockout after repetitive failed login attempts is not configured."
-	echo "Add a call to pam_tally to '${SYSTEM_AUTH_CFG}'. Please see 'man pam_tally' for details."
-	RET=$XCCDF_RESULT_INFORMATIONAL
-    fi
+sed -n -e ':a; /\\$/N; s/\\\n//; ta; s/^[ \t]*auth[^#]*pam_tally\.so.*//p; T; q 1' ${SYSTEM_AUTH_CFG}
+if [[ "$?" -eq "0" ]]; then
+    echo "Account lockout after repetitive failed login attempts is not configured."
+    echo "Add a call to pam_tally to '${SYSTEM_AUTH_CFG}'. Please see 'man pam_tally' for details."
+    RET=$XCCDF_RESULT_FAIL
 fi
 
 exit $RET
-
