3a83ee96e69abcba4fff741ff8e518dd01971213
Viktor Hercinger <vhercing@redhat.com>
2013-04-02 16:24:21 +0200

    typo in telnet check fixed

--- src/services/obsolete/disabled_telnet.sh
+++ src/services/obsolete/disabled_telnet.sh
@@ -2,7 +2,7 @@
 
 RET=${XCCDF_RESULT_PASS}
 
-if rpm -qf telnet-server >/dev/null 2>&1
+if rpm -q telnet-server >/dev/null 2>&1
 then
     echo "The telnet server is installed!"
     exit $XCCDF_RESULT_FAIL
