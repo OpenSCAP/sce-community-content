58f5c6b8b697cf0e89ec28f3991e53413e279c9e
Viktor Hercinger <vhercing@redhat.com>
2013-04-03 15:44:18 +0200

    check existence of files in SSH permission/owner checks

--- src/services/openssh/ssh_pubkey.sh
+++ src/services/openssh/ssh_pubkey.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 RET=$XCCDF_RESULT_PASS
 
 while read user home shell
@@ -6,64 +8,79 @@ do
     then
         continue
     fi
-    if [ "x$(find $home/.ssh -perm /022 -maxdepth 0)" != "x" ]
+    if [ -e $home/.ssh ]
     then
-        echo "$home/.ssh has invalid permissions set"
-        RET=$XCCDF_RESULT_FAIL
+        if [ "x$(find $home/.ssh -perm /022 -maxdepth 0)" != "x" ]
+        then
+            echo "$home/.ssh has invalid permissions set"
+            RET=$XCCDF_RESULT_FAIL
+        fi
+
+        if [ "x$(stat -c '%U' $home/.ssh)" != "x$user" ]
+        then
+            echo "$home/.ssh has invalid owner"
+            RET=$XCCDF_RESULT_FAIL
+        fi
     fi
 
-    if [ "x$(stat -c '%U' $home/.ssh)" != "x$user" ]
+    if [ -e $home/.ssh/authorized_keys ]
     then
-        echo "$home/.ssh has invalid owner"
-        RET=$XCCDF_RESULT_FAIL
+        if [ "x$(find $home/.ssh/authorized_keys -perm /022 -maxdepth 0)" != "x" ]
+        then
+            echo "$home/.ssh/authorized_keys has invalid permissions set"
+            RET=$XCCDF_RESULT_FAIL
+        fi
+
+        if [ "x$(stat -c '%U' $home/.ssh/authorized_keys)" != "x$user" ]
+        then
+            echo "$home/.ssh/authorized_keys has invalid owner"
+            RET=$XCCDF_RESULT_FAIL
+        fi
     fi
 
-    if [ "x$(find $home/.ssh/authorized_keys -perm /022 -maxdepth 0)" != "x" ]
+    if [ -e $home/.ssh/environment ]
     then
-        echo "$home/.ssh/authorized_keys has invalid permissions set"
-        RET=$XCCDF_RESULT_FAIL
+        if [ "x$(find $home/.ssh/environment -perm /022 -maxdepth 0)" != "x" ]
+        then
+            echo "$home/.ssh/environment has invalid permissions set"
+            RET=$XCCDF_RESULT_FAIL
+        fi
+
+        if [ "x$(stat -c '%U' $home/.ssh/environment)" != "x$user" ]
+        then
+            echo "$home/.ssh/environment has invalid owner"
+            RET=$XCCDF_RESULT_FAIL
+        fi
     fi
 
-    if [ "x$(stat -c '%U' $home/.ssh/authorized_keys)" != "x$user" ]
+    if [ -e $home/.ssh/known_hosts ]
     then
-        echo "$home/.ssh/authorized_keys has invalid owner"
-        RET=$XCCDF_RESULT_FAIL
+        if [ "x$(find $home/.ssh/known_hosts -perm /022 -maxdepth 0)" != "x" ]
+        then
+            echo "$home/.ssh/known_hosts has invalid permissions set"
+            RET=$XCCDF_RESULT_FAIL
+        fi
+
+        if [ "x$(stat -c '%U' $home/.ssh/known_hosts)" != "x$user" ]
+        then
+            echo "$home/.ssh/known_hosts has invalid owner"
+            RET=$XCCDF_RESULT_FAIL
+        fi
     fi
 
-    if [ "x$(find $home/.ssh/environment -perm /022 -maxdepth 0)" != "x" ]
+    if [ -e $home/.ssh/rc ]
     then
-        echo "$home/.ssh/environment has invalid permissions set"
-        RET=$XCCDF_RESULT_FAIL
-    fi
+        if [ "x$(find $home/.ssh/rc -perm /022 -maxdepth 0)" != "x" ]
+        then
+            echo "$home/.ssh/rc has invalid permissions set"
+            RET=$XCCDF_RESULT_FAIL
+        fi
 
-    if [ "x$(stat -c '%U' $home/.ssh/environment)" != "x$user" ]
-    then
-        echo "$home/.ssh/environment has invalid owner"
-        RET=$XCCDF_RESULT_FAIL
-    fi
-
-    if [ "x$(find $home/.ssh/known_hosts -perm /022 -maxdepth 0)" != "x" ]
-    then
-        echo "$home/.ssh/known_hosts has invalid permissions set"
-        RET=$XCCDF_RESULT_FAIL
-    fi
-
-    if [ "x$(stat -c '%U' $home/.ssh/known_hosts)" != "x$user" ]
-    then
-        echo "$home/.ssh/known_hosts has invalid owner"
-        RET=$XCCDF_RESULT_FAIL
-    fi
-
-    if [ "x$(find $home/.ssh/rc -perm /022 -maxdepth 0)" != "x" ]
-    then
-        echo "$home/.ssh/rc has invalid permissions set"
-        RET=$XCCDF_RESULT_FAIL
-    fi
-
-    if [ "x$(stat -c '%U' $home/.ssh/rc)" != "x$user" ]
-    then
-        echo "$home/.ssh/rc has invalid owner"
-        RET=$XCCDF_RESULT_FAIL
+        if [ "x$(stat -c '%U' $home/.ssh/rc)" != "x$user" ]
+        then
+            echo "$home/.ssh/rc has invalid owner"
+            RET=$XCCDF_RESULT_FAIL
+        fi
     fi
 
 done <<EOF
--- src/services/openssh/ssh_sensitive.sh
+++ src/services/openssh/ssh_sensitive.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 RET=$XCCDF_RESULT_PASS
 
 while read user home shell
@@ -6,64 +8,79 @@ do
     then
         continue
     fi
-    if [ "x$(find $home/.ssh/identity -perm /077 -maxdepth 0)" != "x" ]
+    if [ -e $home/.ssh/identity ]
     then
-        echo "$home/.ssh/identity has invalid permissions set"
-        RET=$XCCDF_RESULT_FAIL
+        if [ "x$(find $home/.ssh/identity -perm /077 -maxdepth 0)" != "x" ]
+        then
+            echo "$home/.ssh/identity has invalid permissions set"
+            RET=$XCCDF_RESULT_FAIL
+        fi
+
+        if [ "x$(stat -c '%U' $home/.ssh/identity)" != "x$user" ]
+        then
+            echo "$home/.ssh/identity has invalid owner"
+            RET=$XCCDF_RESULT_FAIL
+        fi
     fi
 
-    if [ "x$(stat -c '%U' $home/.ssh/identity)" != "x$user" ]
+    if [ -e $home/.ssh/id_rsa ]
     then
-        echo "$home/.ssh/identity has invalid owner"
-        RET=$XCCDF_RESULT_FAIL
+        if [ "x$(find $home/.ssh/id_rsa -perm /077 -maxdepth 0)" != "x" ]
+        then
+            echo "$home/.ssh/id_rsa has invalid permissions set"
+            RET=$XCCDF_RESULT_FAIL
+        fi
+
+        if [ "x$(stat -c '%U' $home/.ssh/id_rsa)" != "x$user" ]
+        then
+            echo "$home/.ssh/id_rsa has invalid owner"
+            RET=$XCCDF_RESULT_FAIL
+        fi
     fi
 
-    if [ "x$(find $home/.ssh/id_rsa -perm /077 -maxdepth 0)" != "x" ]
+    if [ -e $home/.ssh/id_dsa ]
     then
-        echo "$home/.ssh/id_rsa has invalid permissions set"
-        RET=$XCCDF_RESULT_FAIL
+        if [ "x$(find $home/.ssh/id_dsa -perm /077 -maxdepth 0)" != "x" ]
+        then
+            echo "$home/.ssh/id_dsa has invalid permissions set"
+            RET=$XCCDF_RESULT_FAIL
+        fi
+
+        if [ "x$(stat -c '%U' $home/.ssh/id_dsa)" != "x$user" ]
+        then
+            echo "$home/.ssh/id_dsa has invalid owner"
+            RET=$XCCDF_RESULT_FAIL
+        fi
     fi
 
-    if [ "x$(stat -c '%U' $home/.ssh/id_rsa)" != "x$user" ]
+    if [ -e $home/.ssh/config ]
     then
-        echo "$home/.ssh/id_rsa has invalid owner"
-        RET=$XCCDF_RESULT_FAIL
+        if [ "x$(find $home/.ssh/config -perm /077 -maxdepth 0)" != "x" ]
+        then
+            echo "$home/.ssh/config has invalid permissions set"
+            RET=$XCCDF_RESULT_FAIL
+        fi
+
+        if [ "x$(stat -c '%U' $home/.ssh/config)" != "x$user" ]
+        then
+            echo "$home/.ssh/config has invalid owner"
+            RET=$XCCDF_RESULT_FAIL
+        fi
     fi
 
-    if [ "x$(find $home/.ssh/id_dsa -perm /077 -maxdepth 0)" != "x" ]
+    if [ -e $home/.ssh/random_seed ]
     then
-        echo "$home/.ssh/id_dsa has invalid permissions set"
-        RET=$XCCDF_RESULT_FAIL
-    fi
+        if [ "x$(find $home/.ssh/random_seed -perm /077 -maxdepth 0)" != "x" ]
+        then
+            echo "$home/.ssh/random_seed has invalid permissions set"
+            RET=$XCCDF_RESULT_FAIL
+        fi
 
-    if [ "x$(stat -c '%U' $home/.ssh/id_dsa)" != "x$user" ]
-    then
-        echo "$home/.ssh/id_dsa has invalid owner"
-        RET=$XCCDF_RESULT_FAIL
-    fi
-
-    if [ "x$(find $home/.ssh/config -perm /077 -maxdepth 0)" != "x" ]
-    then
-        echo "$home/.ssh/config has invalid permissions set"
-        RET=$XCCDF_RESULT_FAIL
-    fi
-
-    if [ "x$(stat -c '%U' $home/.ssh/config)" != "x$user" ]
-    then
-        echo "$home/.ssh/config has invalid owner"
-        RET=$XCCDF_RESULT_FAIL
-    fi
-
-    if [ "x$(find $home/.ssh/random_seed -perm /077 -maxdepth 0)" != "x" ]
-    then
-        echo "$home/.ssh/random_seed has invalid permissions set"
-        RET=$XCCDF_RESULT_FAIL
-    fi
-
-    if [ "x$(stat -c '%U' $home/.ssh/random_seed)" != "x$user" ]
-    then
-        echo "$home/.ssh/random_seed has invalid owner"
-        RET=$XCCDF_RESULT_FAIL
+        if [ "x$(stat -c '%U' $home/.ssh/random_seed)" != "x$user" ]
+        then
+            echo "$home/.ssh/random_seed has invalid owner"
+            RET=$XCCDF_RESULT_FAIL
+        fi
     fi
 
 done <<EOF
